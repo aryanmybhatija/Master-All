8a423168fe4e8d3e29ab0debf2d76fb45464df5a
aryanmybhatija <sonutomarlucky@gmail.com>
2025-06-02 10:29:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config(object):
     AUTH_USER = os.environ.get('AUTH_USERS', '6890400066,5744263553').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
-    CREDIT = "꧁•⊹٭𝚂𝚘𝚗𝚞٭⊹•꧂™"#Here You Can Change with Your Name  or any custom name or title you prefer
+    CREDIT = "꧁•⊹٭@𝚂𝚘𝚗𝚞porsa٭⊹•꧂™"#Here You Can Change with Your Name  or any custom name or title you prefer
